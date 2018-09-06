3a6062539346a0b6acd674081f2db6ed25e121ad
Matthew <Matthew.b.rodriguez@gmail.com>
2018-09-06 19:37:52 -0400

    Added Network Request functionality with MovieDB API

---
+++ flix/NowPlayingViewController.swift
@@ -0,0 +1,58 @@
+//
+//  NowPlayingViewController.swift
+//  flix
+//
+//  Created by Matthew on 9/4/18.
+//  Copyright © 2018 Matthew. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingViewController: UIViewController, UITableViewDataSource {
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        
+        // Network Request > JSON data returned > Parse JSON data > Turn into
+        // Swift Dictionary > Access Keys of Dictionary
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                for movie in movies {
+                    let title = movie["title"] as! String
+                    print(title)
+                }
+            }
+        }
+        task.resume()
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        return cell
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+}
